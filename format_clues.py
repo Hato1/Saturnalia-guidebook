8b0b6ea43320f6996666d82ef07d27cf0063ec89
Jacob Rogers <jacob.p.r@hotmail.com>
2022-11-09 01:42:25 +1300

    Added quote boxes to clue map

--- format_clues.py
+++ format_clues.py
@@ -1,5 +1,20 @@
+# with open("clues.txt") as f:
+#     clues = f.read().split("\n\n")
+#     for i, clue in enumerate(clues):
+#         msg = f"\nClue coord {i % 14}, {i // 14}:\n"
+#         print(msg, clue.strip("\n"))
+
+#         print(clue[-1])
+
 with open("clues.txt") as f:
-    clues = f.read().split("\n\n")
-    for i, clue in enumerate(clues):
-        msg = f"\nClue coord {i % 14}, {i // 14}:\n"
-        print(msg, clue.strip("\n"))
+    for i, clue in enumerate(f.read().split("\n\n\n")):
+        coord = f"{i % 14},{i // 14}"
+        clue = clue.split("\n")
+        clue[0] = "<b>" + clue[0] + "</b>"
+
+        quoter = clue[-1].split()[0]
+        if quoter[-1] == ":":
+            quote = open("message_box.html").read()
+            quote = quote.replace("PICTURE_HERE", f"/pfp/{quoter}.png")
+            print(quote)
+            clue.append(quote)
